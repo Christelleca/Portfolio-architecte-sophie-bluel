63e5d908650c502708a6be70a34e6353ae1ef7d6
Christelle CHARLES-ALFRED <christellecharlesalfred56@gmail.com>
2024-02-22 17:25:53 +0100

    modale ajout des works fonctionnelle et style de la modale

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -198,100 +198,111 @@ function displayWorksInContainerGalery(projects = worksData) {
         containerGalery.appendChild(figure);
     });
 
-    // Evénement container_galery //
+    // Evénement suppression des projets //
     containerGalery.addEventListener("click", (e) => {
         const trash = e.target.closest(".fa-trash-can");
         if (trash) {
             const id = trash.dataset.id;
-            deleteWork(id, e);
+            deleteWork(id);
         }
     });
 }
 
 // Modifier la fonction deleteWork pour utiliser l'ID du projet
-function deleteWork(id) {
+
+async function deleteWork(id) {
     const token = localStorage.getItem("token");
 
     const init = {
         method: "DELETE",
         headers: {
-            "Content-Type": "application/json",
             Authorization: `Bearer ${token}`,
         },
     };
 
-    fetch(`http://localhost:5678/api/works/${id}`, init);
-    async (response) => {
-        if (response.ok) {
-            const data = await response.json();
+    const response = await fetch(`http://localhost:5678/api/works/${id}`, init);
+    if (!response.ok) {
+        throw new Error("Erreur lors de la suppression du work");
+    }
 
-            displayWorks();
-            displayProjectAndCategories();
-        }
-    };
+    // Supprimer l'élément du DOM correspondant au work supprimé sur la page d'accueil
+    const figureToDelete = document.querySelector(`.gallery [data-id="${id}"]`);
+    if (figureToDelete) {
+        figureToDelete.remove();
+    }
+
+    // Supprimer l'élément du DOM correspondant au work supprimé dans la modale
+    const modalWorkToDelete = document.querySelector(
+        `.modale_send_work [data-id="${id}"]`
+    );
+    if (modalWorkToDelete) {
+        modalWorkToDelete.remove();
+    }
+
+    // Mettre à jour les données des projets
+    const updatedWorks = await fetchWorks();
+    displayWorksInContainerGalery(updatedWorks);
+    displayWorks(updatedWorks);
 }
 
-// Sélection de l'élément bouton "Ajouter une photo"
+// Variable des éléments d'ajout des nouveaux projets
 const addButton = document.querySelector(".container_modal button");
-
-// Sélection de l'élément "container_modal"
 const modalContainer = document.querySelector(".container_modal");
-
-// Sélection de l'élément "modale_send_work"
 const sendWorkModal = document.querySelector(".modale_send_work");
+const arrowLeftIcon = document.querySelector(".fa-solid.fa-arrow-left");
+const submitButton = document.querySelector(".btn_post_img");
 
-// Ajout d'un écouteur d'événements au clic sur le bouton
+// Ecouteur d'événements au clic sur le bouton "ajouter une photo"
 addButton.addEventListener("click", () => {
-    // Masquer la modale container_modal
     modalContainer.style.display = "none";
-
-    // Afficher la modale modale_send_work
     sendWorkModal.style.display = "flex";
 });
 
-// Sélection de l'icône "fa-xmark"
+// Sélection de l'icône "fa-xmark" de la modale formulaire
 const closeModalIcon = document.querySelector(".contain_form_modale .fa-xmark");
 
-// Ajout d'un écouteur d'événements au clic sur l'icône "fa-xmark"
+// Ecouteur d'événements au clic sur l'icône "fa-xmark"
 closeModalIcon.addEventListener("click", () => {
-    // Masquer la modale modale_send_work
     sendWorkModal.style.display = "none";
 });
 
-// Sélectionner l'icône de flèche gauche
-const arrowLeftIcon = document.querySelector(".fa-solid.fa-arrow-left");
-
-// Ajouter un gestionnaire d'événements pour le clic sur l'icône de flèche gauche
+// Evénements pour le clic sur l'icône de flèche gauche
 arrowLeftIcon.addEventListener("click", () => {
-    // Rendre la modale d'envoi de travail invisible
     const modaleSendWork = document.querySelector(".modale_send_work");
     modaleSendWork.style.display = "none";
-
-    // Rendre la modale principale visible
     const containerModal = document.querySelector(".container_modal");
     containerModal.style.display = "flex";
 });
 
-// Sélection du bouton "+ Ajouter photo"
-const addPhotoButton = document.querySelector(".contain_btn_add_img button");
+// Fonction pour envoyer l'image sélectionnée
+async function uploadImage(file) {
+    const token = localStorage.getItem("token"); // Récupérer le jeton d'authentification depuis le stockage local
+    const formData = new FormData();
+    formData.append("image", file);
 
-// Ajout d'un écouteur d'événements au clic sur le bouton "+ Ajouter photo"
-addPhotoButton.addEventListener("click", () => {
-    // Ouverture de la fenêtre de sélection de fichiers
-    const fileInput = document.createElement("input");
-    fileInput.type = "file";
-    fileInput.accept = "image/jpeg, image/png"; // Définir les types de fichiers acceptés
-    fileInput.onchange = () => handleFileSelection(fileInput.files[0]);
-    fileInput.click();
-});
+    try {
+        const response = await fetch("http://localhost:5678/api/works", {
+            method: "POST",
+            body: formData,
+            headers: {
+                Authorization: `Bearer ${token}`, // Inclure le jeton dans l'en-tête de la requête
+            },
+        });
+        if (!response.ok) {
+            throw new Error("Erreur lors de l'envoi de l'image.");
+        }
+        const imageData = await response.json();
+        return imageData.url; // Renvoyer l'URL de l'image téléchargée
+    } catch (error) {
+        console.error(error);
+        throw new Error("Une erreur est survenue lors de l'envoi de l'image.");
+    }
+}
 
 // Gestion de la sélection de fichier
-function handleFileSelection(file) {
-    const containerImgImport = document.querySelector(".container_img_import");
-    const errorMessage = document.querySelector(".btn_post_img + p.error");
-    const addPhotoButton = document.querySelector(
-        ".contain_btn_add_img button"
-    );
+async function handleFileSelection(event) {
+    const file = event.target.files[0];
+    const errorMessage = document.querySelector(".error_data");
 
     if (file) {
         // Vérification de la taille du fichier
@@ -299,82 +310,142 @@ function handleFileSelection(file) {
             file.size > 4 * 1024 * 1024 ||
             (file.type !== "image/jpeg" && file.type !== "image/png")
         ) {
-            errorMessage.style.display = "block";
             errorMessage.textContent = "Format ou taille du fichier incorrect.";
-            addPhotoButton.style.display = "none";
             return;
         }
 
-        // Affichage de l'aperçu de l'image sélectionnée
-        const reader = new FileReader();
-        reader.onload = (e) => {
-            containerImgImport.innerHTML = `<img src="${e.target.result}" alt="Preview">`;
-            errorMessage.style.display = "none";
-            // Masquer le bouton "Ajouter photo"
-            addPhotoButton.style.display = "none";
-        };
-        reader.readAsDataURL(file);
+        // Envoyer l'image et obtenir son URL
+        try {
+            const imageUrl = await uploadImage(file);
+            // Affichage de l'image téléchargée
+            const imagePreview = document.createElement("img");
+            imagePreview.src = imageUrl;
+            containerBtnAddImg.innerHTML = "";
+            containerBtnAddImg.appendChild(imagePreview);
+        } catch (error) {
+            errorMessage.textContent = error.message;
+        }
     }
 }
 
-// Sélectionnez le bouton "Valider"
-const submitButton = document.querySelector(".btn_post_img");
+// Sélection de l'élément input de type file
+// Événement pour le changement de fichier sélectionné
+const fileInput = document.getElementById("image");
+fileInput.addEventListener("change", function (event) {
+    const previewImage = document.querySelector(".contain_btn_add_img img");
+    const previewForm = document.querySelector(".form_preview_img");
+    const file = event.target.files[0]; // Récupérer le premier fichier sélectionné
+    const reader = new FileReader(); // Créer un objet FileReader
 
-// Ajoutez un gestionnaire d'événements pour le clic sur le bouton "Valider"
+    // Événement lorsque la lecture du fichier est terminée
+    reader.onload = function () {
+        // Mettre à jour la source de l'image avec les données de l'image chargée
+        previewImage.src = reader.result;
+        // Afficher l'aperçu de l'image en mettant la balise img en display block
+        previewImage.style.display = "block";
+        // Masquer le formulaire d'aperçu de l'image
+        previewForm.style.display = "none";
+        // Modifier le style du bouton de validation
+        const submitButton = document.querySelector(".btn_post_img");
+        submitButton.style.background = "#1D6154";
+    };
+
+    if (file) {
+        // Lire le contenu du fichier en tant que URL de données
+        reader.readAsDataURL(file);
+    } else {
+        // Si aucun fichier n'est sélectionné, réinitialiser l'aperçu de l'image
+        previewImage.src = "#";
+        previewImage.style.display = "none"; // Masquer l'aperçu de l'image
+        // Afficher le formulaire d'aperçu de l'image
+        previewForm.style.display = "block";
+        // Réinitialiser le style du bouton de validation
+        const submitButton = document.querySelector(".btn_post_img");
+        submitButton.style.background = "";
+    }
+});
+
+// Ajouter un événement pour le clic sur le bouton de validation
+
 submitButton.addEventListener("click", async (event) => {
-    event.preventDefault(); // Empêche le comportement par défaut du formulaire
+    event.preventDefault();
 
-    // Récupérez les données du formulaire
-    const titre = document.getElementById("titre").value;
-    const categorie = document.getElementById("categorie").value;
-    const image = document.getElementById("image").files[0]; // Récupérez le fichier image
+    // Récupérer les données du formulaire
+    const title = document.getElementById("titre").value;
+    const category = document.getElementById("categorie").value;
+    const imageInput = document.getElementById("image");
+    const image = imageInput.files[0];
 
-    // Vérifiez si tous les champs sont remplis
-    if (!titre || !categorie || !image) {
+    // Vérifier si tous les champs sont remplis
+    if (!title || !category || !image) {
         alert("Veuillez remplir tous les champs.");
         return;
     }
 
-    // Créez un objet FormData
+    const token = localStorage.getItem("token");
     const formData = new FormData();
-    formData.append("titre", titre);
-    formData.append("categorie", categorie);
     formData.append("image", image);
+    formData.append("title", title);
+    formData.append("category", category);
 
-    // Envoyez une requête POST à votre API avec les données du formulaire
-    const response = await fetch("http://localhost:5678/api/works", {
+    const request = {
         method: "POST",
-        body: formData,
         headers: {
-            Authorization: `Bearer ${token}`, // Assurez-vous d'avoir le jeton d'authentification si nécessaire
+            Authorization: `Bearer ${token}`,
         },
-    });
+        body: formData,
+    };
 
-    // Si la requête est réussie, récupérez les données des projets mises à jour
-    const updatedWorks = await response.json();
+    try {
+        const response = await fetch(
+            "http://localhost:5678/api/works",
+            request
+        );
 
-    // Mettez à jour l'affichage des projets avec les données mises à jour
-    displayWorks(updatedWorks);
+        if (response.ok) {
+            // Réinitialiser le formulaire après l'ajout du travail
+            document.getElementById("titre").value = "";
+            document.getElementById("categorie").value = "";
+            imageInput.value = "";
+
+            // Rafraîchir la liste des travaux sur la page d'accueil
+            const updatedWorks = await fetchWorks();
+            displayWorks(updatedWorks);
+            alert(`Votre projet ${title} a été ajouté avec succès.`);
+
+            // Réinitialiser l'aperçu de l'image
+            const previewImage = document.querySelector(
+                ".contain_btn_add_img img"
+            );
+            previewImage.src = "#";
+            previewImage.style.display = "none";
+
+            // Réafficher le formulaire d'aperçu de l'image
+            const previewForm = document.querySelector(".form_preview_img");
+            previewForm.style.display = "flex";
+
+            // Réinitialiser le bouton
+            submitButton.style.background = "";
+        } else {
+            alert("Erreur lors de l'ajout du projet.");
+        }
+    } catch (error) {
+        console.error("Erreur lors de l'ajout du projet :", error);
+    }
 });
 
 // Chargement des catégories
 async function loadCategories() {
     const categoriesSelect = document.getElementById("categorie");
-    categoriesSelect.innerHTML = ""; // Effacer les options existantes
+    categoriesSelect.innerHTML = "";
 
-    try {
-        const response = await fetch("http://localhost:5678/api/categories");
-        const categories = await response.json();
-        categories.forEach((category) => {
-            const option = document.createElement("option");
-            option.value = category.id;
-            option.textContent = category.name;
-            categoriesSelect.appendChild(option);
-        });
-    } catch (error) {
-        console.error("Error loading categories:", error);
-    }
+    const response = await fetch("http://localhost:5678/api/categories");
+    const categories = await response.json();
+    categories.forEach((category) => {
+        const option = document.createElement("option");
+        option.value = category.id;
+        option.textContent = category.name;
+        categoriesSelect.appendChild(option);
+    });
 }
-
-// Appeler la fonction pour charger les catégories
 loadCategories();
